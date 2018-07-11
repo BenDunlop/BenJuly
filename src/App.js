3986b1b570a2291a74d2e29ca794bd25f03b42a6
Ben Dunlop <hello@bendunlop.com>
2018-07-11 13:55:42 +1000

    Todo List Redux Tut

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => (
-  <Button variant="contained" color="primary">
-    Hello World
-  </Button>
-);
- 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App';
 
+const store = createStore(rootReducer)
 
-
-ReactDOM.render( <App />, document.getElementById('app'));
-
-registerServiceWorker();
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,document.getElementById('app')
+)
